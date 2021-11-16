4facc20bd0dcb706e947f61215c790d3b5feb1d9
Omar Bitar <o.bitar97@gmail.com>
2021-11-15 22:39:29 -0700

    added hashing algorithem

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,12 @@
+require 'digest'
 module UsersHelper
     def generate_token
-        token = {
+      secretKey = {
         name:      current_user.name,
         email:     current_user.email,
-        secretKey: current_user.encrypted_password
+        privateKey: current_user.id
       } 
-      token.to_s
+      token = Digest::MD5.hexdigest(secretKey[:name].to_s + secretKey[:email].to_s + secretKey[:privateKey].to_s)  
     #   https://www.sitepoint.com/exploring-cryptography-fundamentals-in-ruby/
     end
 end
