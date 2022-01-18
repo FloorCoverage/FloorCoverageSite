af0400961ac49163eb3ed8948283224561052143
Omar Bitar <o.bitar97@gmail.com>
2022-01-18 15:25:33 -0700

    finished token generation and verfication feature

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,24 @@
 require 'digest'
 module UsersHelper
     def generate_token
-      secretKey = {
-        name:      current_user.name,
-        email:     current_user.email,
-        privateKey: current_user.id
-      } 
-      token = Digest::MD5.hexdigest(secretKey[:name].to_s + secretKey[:email].to_s + secretKey[:privateKey].to_s)  
-    #   https://www.sitepoint.com/exploring-cryptography-fundamentals-in-ruby/
+      publicKey = current_user.id.to_s
+      secretKey = ENV['SECRET_KEY']
+       
+      token = Digest::MD5.hexdigest(publicKey + secretKey) 
     end
+    def match_token(token)
+      # # TODO: remove new line when pasting from site
+      # token = "14ef6c04a0ff59faf19664660ca1be30" 
+      secretKey = ENV['SECRET_KEY']
+      User.all.each do |user|
+        possible_hash = Digest::MD5.hexdigest(user.id.to_s + secretKey)
+        if (possible_hash == token)
+          return user.id
+        end
+      end
+      # TODO: throw pop up not found
+      nil
+    end
+  
 end
  
